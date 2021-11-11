44d8602dd2d6b69ad2f80aec86b24cc3e0b1e770
SEUNGHAN BAEK <84169773+seunghan-baek@users.noreply.github.com>
2021-11-11 14:05:16 +0900

    test commit / gui mod

--- src/GUI/User_GUI.java
+++ src/GUI/User_GUI.java
@@ -1,5 +1,21 @@
 package GUI;
 
-public class User_GUI {
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class User_GUI extends JFrame {
+	JFrame User_P = new JFrame();
+
+	public User_GUI() {
+		init();
+
+	}
+
+	private void init() {
+		this.add(User_P);
+		this.setBounds(100, 100, 300, 100);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	}
 
 }
